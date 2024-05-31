d5a5e5900bde84946201ed390d947e7f39cdc824
shram <shram2615@gmail.com>
2024-05-31 22:58:05 +0300

    11

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,22 +1,22 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-// Використовуємо деструктуризацію, щоб одразу отримати посилання на елементи форми
-const { formEl, inptElemMs } = document.querySelector('.form');
+
+const formEl = document.querySelector('.form');
+const inptElemMs = formEl.querySelector('input[name="delay"]');
 
 formEl.addEventListener('submit', e => {
     e.preventDefault();
     
     const delayValue = inptElemMs.value;
     // Використовуємо оператор об'єктної властивості, це дозволяє уникнути помилок,якщо елемент не знайдено
-    const state = document.querySelector("input[name='state']:checked")?.value;
-// створення обіцянки
-    let promise = new Promise((resolve, reject) => setTimeout(() => {
-        if (state === "fulfilled") resolve(delayValue);
-        else if (state === "rejected") reject(delayValue);
+    const state = formEl.querySelector("input[name='state']:checked")?.value;
+    // створення обіцянки
+    new Promise((resolve, reject) => setTimeout(() => {
+        state === "fulfilled" ? resolve(delayValue) : reject(delayValue);
     }, delayValue))
         // Коли дiя промісу успiшниа в (resolved) передаюсться в обробник (then) буде викликаний i виконаний.
-    .then((delay) => {
+    .then(delay => {
         iziToast.success({
             title: 'OK',
             titleColor: '#fff',
@@ -28,7 +28,7 @@ formEl.addEventListener('submit', e => {
         });
     })
         // Помилки, які виникають (rejected) передаюсться в обробник (catch) буде викликаний i виконаний.
-    .catch((delay) => {
+    .catch(delay => {
         iziToast.error({
             title: 'Error',
             titleColor: '#fff',
